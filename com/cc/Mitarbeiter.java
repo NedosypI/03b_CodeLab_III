87bc8aa0343cec546b6586bd0a2ba178168f97ef
NedosypI <irinanedosyp@gmx.de>
2022-07-26 21:24:10 +0200

    Klassenerstellung

---
+++ com/cc/Mitarbeiter.java
@@ -0,0 +1,43 @@
+package com.cc;
+
+public class Mitarbeiter {
+
+// a.) 
+private String Name, Vorname; 
+private int Geburtsjahr;
+
+// b.)
+public String Mitarbeiternr, Abteilung, Job;
+
+
+
+// Getter / Setter 
+
+public String getName() {
+    return Name;
+}
+public void setName(String name) {
+    Name = name;
+}
+public String getVorname() {
+    return Vorname;
+}
+public void setVorname(String vorname) {
+    Vorname = vorname;
+}
+public int getGeburtsjahr() {
+    return Geburtsjahr;
+}
+public void setGeburtsjahr(int geburtsjahr) {
+    Geburtsjahr = geburtsjahr;
+} 
+
+public void hasLunch() {
+}
+
+public void startsWork() {
+}
+
+
+
+}
